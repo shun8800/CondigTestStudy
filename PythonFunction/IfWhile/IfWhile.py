86c1532cd5e53873d8e3815c4377dc897da30dff
이승헌 <shun8800@naver.com>
2021-08-06 01:14:59 +0900

    파이썬 문법(반복문 조건문)

---
+++ PythonFunction/IfWhile/IfWhile.py
@@ -0,0 +1,29 @@
+# in 연산자와 not in 연산자(포함 관련, 하나 씩 방문)
+# 파이썬은 부등호를 0 < x < 20 이런식으로 표현 가능
+'''
+array = [9, 8, 7, 6, 5]
+tup = (1, 2, 3, 4, 5)
+
+for x in tup:
+    print(x)
+
+for x in array:
+    print(x)
+
+# 1부터 9까지 순회하는 값
+
+result = 0
+for x in range(1, 10):
+    result += x
+
+print(result)
+'''
+
+scores = [90, 52, 34, 14, 62]
+cheating = {2, 4}
+
+for i in range(5):
+    if i+1 in cheating:
+        continue # 조건문에 해당되면 뛰어넘기
+    if scores[i]>=20:
+        print(i+1, "번 학생 합격")
--- PythonFunction/InputOutput/Function.py
+++ PythonFunction/InputOutput/Function.py
@@ -9,21 +9,36 @@
 
 '''
 n = int(input())
-data = list(map(int, input().split()))
-a, b, c = list(map(int, input().split()))
+data_1 = input().split()
+data_2 = map(int, data_1) # data2 = map(int, input().split())
+data_3 = list(data_2) # list(map(int, input().split())
 print(n)
-print(data)
-print(a, b, c)
+print(data_1)
+print(data_2)
+print(data_3)
 '''
+
 
 
 # 사용자로부터 입력을 빠르게 받아야 하는 경우
 # sys.stdin.readline() 메서드를 이용
 # 엔터가 줄 바꿈 기호이므로 rstrip() 메서드를 사용
 
+'''
 import sys
 
 data = sys.stdin.readline().rstrip() # input() 과 똑같은 역할인듯?
 data1 = list(map(int, data.split()))
 print(data)
 print(data1)
+'''
+
+# print는 기본적으로 출력 후에 개행을 함
+# 개행을 피하고 싶으면 end = " " 추가
+
+a = 1
+b = 2
+print(a)
+print(b)
+print(7, end=" ")
+print(8, end=" ")
